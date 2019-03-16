46eb5bb8275ea5f4bc7aa14864c23d898a46d0cc
genpati <genpati@pop-os.localdomain>
2019-03-16 19:57:43 +0700

    remove app2

--- app.py
+++ app.py
@@ -1,32 +1,78 @@
 import os
-from flask import Flask, request, redirect, render_template, url_for, flash, abort, g
-from flask_uploads import UploadSet, config_for_set, IMAGES, UploadNotAllowed, configure_uploads
+from flask import Flask, render_template, redirect, url_for, request
+from wtforms import SubmitField, StringField
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOADS_DEFAULT_DEST'] = 'static/var/uploads'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/db-images'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'a-random-string'
 
-photos = UploadSet('photos', IMAGES)
-#media = UploadSet('media', default_dest=lambda app: app.instance_root)
-configure_uploads(app, (photos))
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        rec = Photo(filename=filename)
-        rec.store()
-        flash("Photo saved.")
-        return redirect(url_for('show', id=rec.id))
-    return render_template('upload.html')
+class PhotoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    photo_filename = db.Column(db.String(254))
 
-@app.route('/photo/<id>')
-def show(id):
-    photo = Photo.load(id)
-    if photo is None:
-        abort(404)
-    url = photos.url(photo.filename)
-    return render_template('show.html', url=url, photo=photo)
+    def __repr__(self):
+        return '<Filename %r>' % self.photo_filename
+
+class PhotoForm(FlaskForm):
+    name = StringField(u'Kategori', validators=[DataRequired()])
+    photo = FileField(u'Photo', validators=[FileRequired(), FileAllowed(['jpeg','jpg','png'], 'Images only!')])
+    submit = SubmitField(u'Simpan')
+
+@app.route('/')
+def index():
+    form = PhotoForm()
+    return render_template('upload.html', form=form)
+
+@app.route('/unggah', methods=['GET', 'POST'])
+def unggah(): 
+    form = PhotoForm()
+    target = os.path.join(app.static_folder, 'uploads/photos')
+    print("Target Folder : " + target)
+    
+    if not os.path.exists(target):
+        print("Make Directory!")
+        os.makedirs(target)
+    
+    if form.validate_on_submit(): 
+        file = form.photo.data.filename
+        print(file)
+        filename = secure_filename(form.photo.data.filename)
+        destination = "/".join([target, filename])
+        print("Filename : "+filename)
+        print("Destination : "+destination)
+        form.photo.data.save(destination)
+
+        photo = PhotoModel(photo_filename=filename, name=form.name.data)
+        db.session.add(photo)
+        db.session.commit()
+
+        return redirect('lihat')
+
+    return render_template('upload.html', form=form)
+
+@app.route('/lihat')
+def lihat():
+    photos = PhotoModel.query.all()
+    return render_template('show.html', photos=photos)
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    form = PhotoForm()
+    data = PhotoModel.query.filter_by(id=id).first_or_404()
+    form.name.data = data.name
+    form.photo.data = data.photo_filename
+    return render_template('upload.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
