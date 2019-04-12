9b45f6d4791994eb4743b73786a0d0142a4f44e9
David Rigan <david@dvrg.me>
2019-04-13 00:38:30 +0700

    fix save image to db

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os, uuid
 from io import BytesIO
-from flask import Flask, render_template, redirect, url_for, request, send_file
+from flask import Flask, render_template, redirect, url_for, request, send_file, make_response, send_from_directory
 from wtforms import SubmitField, StringField
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
@@ -8,6 +8,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
+from base64 import b64encode
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/db-images'
@@ -19,15 +20,23 @@ migrate = Migrate(app, db)
 
 target = os.path.join(app.static_folder, 'uploads/photos')
 
+def unique_name(data):
+    file = data.filename
+    ext = file.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4().hex, ext)
+    return filename
+
 class PhotoModel(db.Model):
+    __maxsize__ = 4096
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140))
     photo_filename = db.Column(db.String(254))
+    photo = db.Column(db.LargeBinary(__maxsize__))
     doc_filename = db.Column(db.String(254))
     doc = db.Column(db.LargeBinary)
 
     def __repr__(self):
-        return '<Filename %r>' % self.photo_filename
+        return '<ID %r>' % self.id
 
 class PhotoForm(FlaskForm):
     name = StringField(u'Kategori', validators=[DataRequired()])
@@ -38,21 +47,9 @@ class PhotoForm(FlaskForm):
 @app.route('/', methods=['GET','POST'])
 def unggah(): 
     form = PhotoForm()
-    print("Target Folder : " + target)
-    
-    if not os.path.exists(target):
-        print("Make Directory!")
-        os.makedirs(target)
-    
     if form.validate_on_submit(): 
-        doc = form.doc.data
-        file = form.photo.data.filename
-        ext = file.split('.')[-1]
-        filename = "%s.%s" % (uuid.uuid4().hex, ext)
-        destination = "/".join([target, filename])
-        form.photo.data.save(destination)
 
-        photo = PhotoModel(photo_filename=filename, name=form.name.data, doc_filename=doc.filename, doc=doc.read())
+        photo = PhotoModel(photo_filename=unique_name(form.photo.data), photo=form.photo.data.read(), name=form.name.data, doc_filename=unique_name(form.doc.data), doc=form.doc.data.read())
         db.session.add(photo)
         db.session.commit()
 
@@ -60,57 +57,42 @@ def unggah():
 
     return render_template('upload.html', form=form)
 
-@app.route('/lihat', methods=['GET'])
+@app.route('/lihat')
 def lihat():
-    photos = PhotoModel.query.all()
-    return render_template('show.html', photos=photos)
+    data = PhotoModel.query.all()
+    return render_template('show.html', data=data)
 
-@app.route('/lihat/<int:id>', methods=['GET'])
-def lihat_data(id):
-    file_data = PhotoModel.query.filter_by(id=id).first_or_404()
+@app.route('/images/<string:filename>', methods=['GET'])
+def load_gambar3(filename):
+    image_data = PhotoModel.query.filter_by(photo_filename=filename).first_or_404()
+    return send_file(BytesIO(image_data.photo), mimetype='images/generic', as_attachment=True, attachment_filename=image_data.photo_filename)
+    #return send_from_directory(directory=BytesIO(image_data.photo), filename=image_data.photo_filename, mimetype='images/generic')
+
+@app.route('/lihat/<string:filename>', methods=['GET'])
+def lihat_data(filename):
+    file_data = PhotoModel.query.filter_by(doc_filename=filename).first_or_404()
     return send_file(BytesIO(file_data.doc), attachment_filename=file_data.doc_filename, mimetype='application/pdf')
 
 @app.route('/edit/<int:id>', methods=['GET','POST'])
 def edit(id):
-    print("Target Folder : " + target)
 
     form = PhotoForm()
     data = PhotoModel.query.filter_by(id=id).first_or_404()
     
     if form.validate_on_submit():
-
-        if data.doc_filename is not None:
-            old_doc = data.doc_filename
-            destination_del = "/".join([target, old_doc])
-            if os.path.exists(destination_del):
-                os.remove(destination_del)
-
-        if data.photo_filename is not None:
-            old = data.photo_filename
-            destination_del = "/".join([target, old])
-            if os.path.exists(destination_del):
-                os.remove(destination_del)
-
-        file2 = form.doc.data
-        file = form.photo.data.filename
-        ext = file.split('.')[-1]
-        filename = "%s.%s" % (uuid.uuid4().hex, ext)
-        destination_save = "/".join([target, filename])
-        form.photo.data.save(destination_save)
-
-        data.photo_filename=filename
         data.name=form.name.data
-        data.doc_filename=file2.filename
-        data.doc=file2.read()
-
+        data.photo=form.photo.data.read()
+        data.photo_filename=unique_name(form.photo.data)
+        data.doc=form.doc.data.read()
+        data.doc_filename=unique_name(form.doc.data)
+        
         db.session.commit()
         return redirect('lihat')
-    #else:
-        #filename = None
     
     form.name.data = data.name
-    form.photo.data = data.photo_filename
-
+    #form.photo.data = send_file(BytesIO(data.photo), attachment_filename=data.photo_filename, as_attachment=True)
+    #form.doc.data = data.doc
+    
     return render_template('upload.html', form=form)
 
 @app.route('/hapus/<int:id>', methods=['GET','POST'])
